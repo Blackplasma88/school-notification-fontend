023d9a4b94f5b2b19fbbd0752850b174df71c6da
armitsa <itsaraphap.s@ku.th>
2023-04-14 21:09:08 +0700

    update class complete

--- src/components/classdata/ReadClassComponent.vue
+++ src/components/classdata/ReadClassComponent.vue
@@ -3,60 +3,66 @@
     <div>
       <!-- {{ classData }} -->
       <h4>ชั้นปี : ม.{{ classData.class_year }}/ {{ classData.class_room }}</h4>
-      <h4>อาจารย์ที่ปรึกษา : {{ classData.advisor_id }}</h4>
+      <h4>อาจารย์ที่ปรึกษา : {{ this.advisor_name }}</h4>
       <h4>ปีการศึกษา : {{ classData.year }}</h4>
       <h4>ภาคการศึกษา : {{ classData.term }}</h4>
       <h4>จำนวนนักเรียน : {{ classData.number_of_student }}</h4>
-      <table class="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th scope="col">รหัสนักเรียน</th>
-            <th scope="col">ชื่อนักเรียน</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(id, name) in classData.student_id_list" :key="id">
-            <td>
-              {{ id }}
-            </td>
-            <td>{{ this.student_name_list[name] }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <h4>ตารางเรียน</h4>
-      <table class="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th scope="col">วัน</th>
-            <th scope="col">8.30 - 9.00</th>
-            <th scope="col">9.00 - 9.30</th>
-            <th scope="col">9.30 - 10.00</th>
-            <th scope="col">10.00 - 10.30</th>
-            <th scope="col">10.30 - 11.00</th>
-            <th scope="col">12.00 - 12.30</th>
-            <th scope="col">12.30 - 13.00</th>
-            <th scope="col">13.00 - 13.30</th>
-            <th scope="col">13.30 - 14.00</th>
-            <th scope="col">14.00 - 14.30</th>
-            <th scope="col">14.30 - 15.00</th>
-            <th scope="col">15.00 - 15.30</th>
-            <th scope="col">15.30 - 16.00</th>
-            <th scope="col">16.00 - 16.30</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="slot in classData.slot" :key="slot.day">
-            <td>
-              <div>
-                {{ slot.day }}
-              </div>
-            </td>
-            <td v-for="time in slot.time_slot" :key="time.time">
-              <div v-if="time.status == true" style="color: green">In Use</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-if="classData.number_of_student != 0">
+        <table class="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">รหัสนักเรียน</th>
+              <th scope="col">ชื่อนักเรียน</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(id, name) in classData.student_id_list" :key="id">
+              <td>
+                {{ id }}
+              </td>
+              <td>{{ this.student_name_list[name] }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-if="classData.slot != null">
+        <h4>ตารางเรียน</h4>
+        <table class="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">วัน</th>
+              <th scope="col">8.30 - 9.00</th>
+              <th scope="col">9.00 - 9.30</th>
+              <th scope="col">9.30 - 10.00</th>
+              <th scope="col">10.00 - 10.30</th>
+              <th scope="col">10.30 - 11.00</th>
+              <th scope="col">12.00 - 12.30</th>
+              <th scope="col">12.30 - 13.00</th>
+              <th scope="col">13.00 - 13.30</th>
+              <th scope="col">13.30 - 14.00</th>
+              <th scope="col">14.00 - 14.30</th>
+              <th scope="col">14.30 - 15.00</th>
+              <th scope="col">15.00 - 15.30</th>
+              <th scope="col">15.30 - 16.00</th>
+              <th scope="col">16.00 - 16.30</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="slot in classData.slot" :key="slot.day">
+              <td>
+                <div>
+                  {{ slot.day }}
+                </div>
+              </td>
+              <td v-for="time in slot.time_slot" :key="time.time">
+                <div v-if="time.status == true" style="color: green">
+                  In Use
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -91,6 +97,7 @@ export default {
         ],
       },
       student_name_list: [],
+      advisor_name: "",
     };
   },
   mounted() {
@@ -103,6 +110,18 @@ export default {
         this.classData = response.data.data.class;
         console.log("classData", this.classData);
         console.log("student_id_list", this.classData.student_id_list);
+
+        axios
+          .get(
+            "http://127.0.0.1:8080/profile/id?id=" +
+              this.classData.advisor_id +
+              "&role=teacher"
+          )
+          .then((response) => {
+            console.log("advisor");
+            console.log("name", response.data.data.profile.name);
+            this.advisor_name = response.data.data.profile.name;
+          });
 
         for (var i = 0; i < this.classData.student_id_list.length; i++) {
           let indexI = i;
